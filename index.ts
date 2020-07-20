9b06cb2a8db7a2acf2f7fb55456f3ad406449a3d
risingbirdsong <heregoingthere@outlook.com>
2020-07-20 12:38:36 -0700

    a history of where the snake has been after it ate itself according to color

--- index.ts
+++ index.ts
@@ -10,13 +10,22 @@ const App = new p5((s: p5) => {
   let scoreP: p5.Element;
   const foodArr: { x: number, y: number }[] = [];
   let foodCoords: { x: number, y: number };
+  let active: boolean = false;
+  let death: boolean = false;
   let snake: Snake;
+  let restartButton: p5.Element;
   s.setup = () => {
     s.createCanvas(500, 500);
     s.createP("score").center("horizontal").addClass("scoreLabel");
     scoreP = s.createP(score.toString())
     scoreP.center("horizontal");
     s.frameRate(15);
+    restartButton = s.createButton("restart label", "restart");
+
+    restartButton.mouseClicked(() => {
+      console.log("restart please");
+      death = false;
+    })
     spawnSnake();
     makeFood();
     makeFood();
@@ -36,16 +45,24 @@ const App = new p5((s: p5) => {
     // foodCoords = { x: col, y: row }
   }
   const snakeEatsItself = () => {
+    restartButton.show();
     for (let i = 0; i < snake.tail.length - 1; i++) {
       let t = snake.tail[i];
       if (t.x === snake.x && t.y === snake.y) {
-        snake = null;
-        spawnSnake();
-        resetScore();
+        // snake = null;
+        if (active === true) {
+          death = true;
+        }
+        // spawnSnake();
+        // resetScore();
       }
     }
   }
-  const resetScore = () => {
+  const resetScore = async () => {
+
+    await new Promise((res, rej) => {
+      setTimeout(res, 2000);
+    })
     score = 0;
     scoreP.remove();
     scoreP = s.createP(score.toString());
@@ -55,6 +72,7 @@ const App = new p5((s: p5) => {
     for (let i = 0; i < foodArr.length; i++) {
       let f = foodArr[i];
       if (f.x === snake.x && f.y === snake.y) {
+        active = true;
         score++;
         scoreP.value(score);
         scoreP.remove();
@@ -70,29 +88,51 @@ const App = new p5((s: p5) => {
     snake.steer(s.keyCode)
   }
   s.draw = () => {
-    s.background(230);
-    eat();
-    snakeEatsItself();
-    snake.update();
-    snake.draw();
-    for (let f of foodArr) {
-      s.fill(10, 255, 10);
-      s.rect(f.x, f.y, squareSide, squareSide);
+    if (death === false) {
+      s.background(230);
+      eat();
+      snakeEatsItself();
+      snake.update();
+      snake.draw();
+      for (let f of foodArr) {
+        s.fill(10, 255, 10);
+        s.rect(f.x, f.y, squareSide, squareSide);
+      }
+      if (snake.x < 0 || snake.x > s.width) {
+        snake = null;
+        spawnSnake();
+        resetScore();
+      }
+      else if (snake.y < 0 || snake.y > s.height) {
+        snake = null;
+        spawnSnake();
+        resetScore();
+      }
+      for (let i = 0; i < s.width; i += squareSide) {
+        s.line(i, 0, i, s.height);
+        for (let j = 0; j < s.height; j += squareSide) {
+          s.line(0, i, s.width, i);
+        }
+      }
     }
-    if (snake.x < 0 || snake.x > s.width) {
-      snake = null;
-      spawnSnake();
-      resetScore();
-    }
-    else if (snake.y < 0 || snake.y > s.height) {
-      snake = null;
-      spawnSnake();
-      resetScore();
-    }
-    for (let i = 0; i < s.width; i += squareSide) {
-      s.line(i, 0, i, s.height);
-      for (let j = 0; j < s.height; j += squareSide) {
-        s.line(0, i, s.width, i);
+    else if (death === true) {
+      s.background(230);
+      for (let i = 0; i < s.width; i += squareSide) {
+        s.line(i, 0, i, s.height);
+        for (let j = 0; j < s.height; j += squareSide) {
+          s.line(0, i, s.width, i);
+        }
+      }
+      for (let i = 0; i < snake.snakehistory.length; i++) {
+        let highVal = snake.snakehistory[snake.snakehistory.length - 1].history;
+        let hstry = snake.snakehistory[i];
+        let mapped = s.map(hstry.history, 0, highVal, 50, 255);
+        let redMapped = s.map(hstry.history, 0, highVal, 150, 255);
+        let blu = Math.floor(mapped);
+        let red = Math.floor(redMapped)
+        s.fill(red, 0, blu);
+        s.stroke(1);
+        s.rect(hstry.x, hstry.y, squareSide, squareSide);
       }
     }
   }
